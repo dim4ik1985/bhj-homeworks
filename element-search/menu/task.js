87a7e8be53580cb43ceb0e3a70a72a21877f289f
dim4ik85 <dim4ik1985@yandex.ru>
2023-01-26 21:19:54 +0300

    edit2 task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,7 +3,7 @@ const menuLink = document.querySelectorAll('.menu__link');
 Array.from(menuLink).forEach(item => item.onclick = () => {
 
     if (!item.nextElementSibling.className.includes('menu_active')) {
-        closeAllSubMenu();
+        // closeAllSubMenu();
         item.closest('.menu__item').lastElementChild.classList.add('menu_active')
 
         return false;
@@ -21,43 +21,6 @@ function closeAllSubMenu(){
     Array.from(subMenu).forEach(item => item.classList.remove('menu_active'))
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     menuSub = document.querySelectorAll('.menu_sub');
-//
-//
-// for (let i=0; i < menuLink.length; i++) {
-//     const menuItem = menuLink[i].closest('.menu__item');
-//
-//     menuLink[i].onclick = () => {
-//         // menuSub.forEach(elem => {
-//         //     elem.classList.toggle('menu_active');
-//         //     }
-//         // );
-//
-//         if (menuItem.querySelector('.menu_sub')) {
-//             console.log(menuItem.querySelector('.menu_sub'))
-//             menuItem.querySelector('.menu_sub').classList.toggle('menu_active');
-//             return false
-//         }
-//     }
-// }
 
 
 
